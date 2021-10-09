33157c47a30197db2f0c7fc6ec7131127a588df8
Fiskless <Fiskless@gmail.com>
2021-10-09 23:30:18 +0500

    Add silence to vkbot

--- vk_bot.py
+++ vk_bot.py
@@ -38,11 +38,12 @@ def detect_intent_texts(event, vk_api, language_code='ru-RU'):
     print("Fulfillment text: {}\n".format(
         response.query_result.fulfillment_text))
 
-    vk_api.messages.send(
-        user_id=event.user_id,
-        message=response.query_result.fulfillment_text,
-        random_id=random.randint(1, 1000)
-    )
+    if not response.query_result.intent.is_fallback:
+        vk_api.messages.send(
+            user_id=event.user_id,
+            message=response.query_result.fulfillment_text,
+            random_id=random.randint(1, 1000)
+        )
 
 
 def main():
