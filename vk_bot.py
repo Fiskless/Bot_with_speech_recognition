38a517c2d8b5a3d5f30f4e6d21d32762bcb3f3d4
Fiskless <Fiskless@gmail.com>
2021-10-09 23:25:50 +0500

    Create VK bot and connect DialogFlow phrases

---
+++ vk_bot.py
@@ -0,0 +1,61 @@
+import os
+import random
+import vk_api as vk
+
+from dotenv import load_dotenv
+from google.cloud import dialogflow
+from vk_api.longpoll import VkLongPoll, VkEventType
+
+
+def detect_intent_texts(event, vk_api, language_code='ru-RU'):
+
+    session_id = str(event.user_id)
+    text = event.text
+    project_id = os.getenv('DIALOG_FLOW_PROJECT_ID')
+
+    session_client = dialogflow.SessionsClient()
+
+    session = session_client.session_path(project_id, session_id)
+    print("Session path: {}\n".format(session))
+
+    text_input = dialogflow.TextInput(text=text,
+                                      language_code=language_code)
+
+    query_input = dialogflow.QueryInput(text=text_input)
+
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+    )
+
+    print("=" * 20)
+    print("Query text: {}".format(response.query_result.query_text))
+    print(
+        "Detected intent: {} (confidence: {})\n".format(
+            response.query_result.intent.display_name,
+            response.query_result.intent_detection_confidence,
+        )
+    )
+    print("Fulfillment text: {}\n".format(
+        response.query_result.fulfillment_text))
+
+    vk_api.messages.send(
+        user_id=event.user_id,
+        message=response.query_result.fulfillment_text,
+        random_id=random.randint(1, 1000)
+    )
+
+
+def main():
+    load_dotenv()
+    vk_group_token = os.getenv("VK_GROUP_TOKEN")
+    vk_session = vk.VkApi(token=vk_group_token)
+
+    vk_api = vk_session.get_api()
+    longpoll = VkLongPoll(vk_session)
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            detect_intent_texts(event, vk_api)
+
+
+if __name__ == '__main__':
+    main()
